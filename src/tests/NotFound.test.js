15fb14d2969ab51e298bfdf3ab2806ea8e524080
Vinicius José <viniciusjosedev@gmail.com>
2023-01-20 17:12:22 -0300

    requeriment 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    expect(screen.getByRole('heading', { name: /page requested not found/i })).toBeInTheDocument();
+    expect(screen.getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    ).src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
