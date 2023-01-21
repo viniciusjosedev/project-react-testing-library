92b27af737a13355be163ba90837ee596a9791b3
Vinicius José <viniciusjosedev@gmail.com>
2023-01-20 23:17:43 -0300

    requeriment 5 done

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,25 +2,30 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import Pokedex from '../pages/Pokedex';
 import renderWithRouter from '../renderWithRouter';
 import pokemonList from '../data';
 
 describe('Teste o componente <Pokedex.js />', () => {
   const pokemonName = 'pokemon-name';
   it('Teste se a página contém um heading h2 com o texto Encountered Pokémon;', () => {
-    renderWithRouter(<App />);
-
+    // renderWithRouter(<Pokedex pokemonList={ pokemonList } isPokemonFavoriteById={ [] } />);
+		renderWithRouter(<App />);
     expect(screen.getByRole('heading', { name: /encountered pokémon/i })).toBeInTheDocument();
   });
 
   it('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
     renderWithRouter(<App />);
 
+    userEvent.click(screen.getByRole('button', { name: /fire/i }));
+    userEvent.click(screen.getByRole('button', { name: /all/i }));
     pokemonList.forEach(async (elemento) => {
       userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
       expect(await screen.findByText(elemento.name).innerText)
         .toEqual(screen.getByTestId(pokemonName).innerText);
     });
+    userEvent.click(screen.getByRole('button', { name: /all/i }));
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
   });
 
   it('Teste se é mostrado apenas um Pokémon por ve', () => {
@@ -91,7 +96,5 @@ describe('Teste o componente <Pokedex.js />', () => {
       expect(screen.getByTestId(pokemonName).innerHTML).toBe(elemento.name);
       userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
     });
-
-    expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
   });
 });
